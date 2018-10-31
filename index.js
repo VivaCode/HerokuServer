690e50b73b853f6d6d11cb67789706d8649e3402
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-31 11:29:13 -0500

    basic server created

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 8000;
+const instance = server.listen(port, () =>
+	console.log(`\n=== Server running on port: ${instance.address().port} ===\n`)
+);
