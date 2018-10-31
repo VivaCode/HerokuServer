01b8d8789690c0ffa9e32c22ba244d51a86bdd1a
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-31 15:55:36 -0500

    json added index modified.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;//makes the port dynamic
 const instance = server.listen(port, () =>
 	console.log(`\n=== Server running on port: ${instance.address().port} ===\n`)
 );
